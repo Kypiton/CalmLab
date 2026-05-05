7a3d032daa471ecdcff4d09102f936257ee0d7b3
Dan <partytimeyep@gmail.com>
2026-05-05 17:09:31 +0300

    Make home page public

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,20 +1,14 @@
 import { Header, Hero, Products } from '@/components/shared';
-import { getCurrentUser } from '@/lib/auth';
 import prisma from '@/lib/prisma';
-import { redirect } from 'next/navigation';
 
 export default async function Home() {
   const products = (await prisma.product.findMany()).slice(0, 8);
-  const user = await getCurrentUser();
 
-  if (!user) {
-    redirect('/sign-in');
-  } else
-    return (
-      <>
-        <Header />
-        <Hero />
-        <Products products={products} />
-      </>
-    );
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Products products={products} />
+    </>
+  );
 }
