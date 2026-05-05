ca14ff83af40fb062a006befd8d15161a32e834c
Dan <partytimeyep@gmail.com>
2026-05-05 17:29:03 +0300

    Make home page dynamic

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,8 +1,12 @@
 import { Header, Hero, Products } from '@/components/shared';
 import prisma from '@/lib/prisma';
 
+export const dynamic = 'force-dynamic';
+
 export default async function Home() {
-  const products = (await prisma.product.findMany()).slice(0, 8);
+  const products = await prisma.product.findMany({
+    take: 8,
+  });
 
   return (
     <>
